e07a09dfc253de1812a4a76f7b95831ee37376ca
Danil <volkdana78@gmail.com>
2023-09-11 14:22:28 +0300

    workingShop

--- main.js
+++ main.js
@@ -2,23 +2,23 @@ const iphone = document.querySelector('.iphone');
 const samsung = document.querySelector('.samsung');
 
 iphone.addEventListener("click", function (event) {
-    let clickTaregt = event.target;
+    let clickTarget = event.target;
     let activeButton = iphone.querySelector(".active");
     let priceItem = iphone.querySelector(".price");
     let currentPrice;
     let activeButtonColor = iphone.querySelector(".active-border");
 
-    if (clickTaregt.classList.contains("color-bth") && 
-        !clickTaregt.classList.contains("active-border")) {
-        clickTaregt.classList.add("active-border");
-        activeButton.Color.classList.remove("active-border");
+    if (clickTarget.classList.contains("color-btn") && 
+        !clickTarget.classList.contains("active-border")) {
+            clickTarget.classList.add("active-border");
+        activeButtonColor.classList.remove("active-border");
     }
-    if (clickTaregt.classList.contains("bth") &&
-        !clickTaregt.classList.contains("active")) {
-            clickTaregt.classList.add("active");
+    if (clickTarget.classList.contains("btn") &&
+        !clickTarget.classList.contains("active")) {
+            clickTarget.classList.add("active");
             activeButton.classList.remove("active");
         
-        currentPrice = clickTaregt.getAttribute("data-size");
+        currentPrice = clickTarget.getAttribute("data-size");
 
         if(currentPrice === "medium") {
             priceItem.textContent = "Цена 80 000р.";
@@ -38,13 +38,13 @@ samsung.addEventListener("click", function(event)  {
     let currentPrice;
     let activeButtonColor = samsung.querySelector(".active-border");
 
-    if (clickTarget.classList.contains("color-bth") && 
+    if (clickTarget.classList.contains("color-btn") && 
     !clickTarget.classList.contains("active-border")) {
     clickTarget.classList.add("active-border");
-    activeButton.Color.classList.remove("active-border");
+    activeButtonColor.classList.remove("active-border");
 }
 
-if (clickTarget.classList.contains("bth") &&
+if (clickTarget.classList.contains("btn") &&
 !clickTarget.classList.contains("active")) {
         clickTarget.classList.add("active");
         activeButton.classList.remove("active");
